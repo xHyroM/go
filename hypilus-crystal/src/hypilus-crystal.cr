071d1aaf6c81ac7851433431137727fa260d6f45
xHyroM <generalkubo@gmail.com>
2022-08-05 19:16:24 +0200

    feat: implement error handling in POST /api/books

--- hypilus-crystal/src/hypilus-crystal.cr
+++ hypilus-crystal/src/hypilus-crystal.cr
@@ -25,17 +25,21 @@ before_get do |env|
   env.response.content_type = "application/json"
 end
 
-get "/api/list" do
+get "/api/books" do
   books.to_json
 end
 
-post "/api/book" do |env|
-  name = env.params.json["name"].as(String)
-  description = env.params.json["description"].as(String)
-  author = env.params.json["author"].as(String)
-  price = env.params.json["price"].as(Float64)
+post "/api/books" do |env|
+  name = env.params.json["name"]?
+  description = env.params.json["description"]?
+  author = env.params.json["author"]?
+  price = env.params.json["price"]?
 
-  book = Book.new(name, description, author, price)
+  if name.nil? || description.nil? || author.nil? || price.nil?
+    next {"error": "Bad Request", "message": "Missing body"}.to_json
+  end
+
+  book = Book.new(name.as(String), description.as(String), author.as(String), price.as(Float64))
   if books.includes?(book)
     env.response.status_code = 400
     {"error": "Bad Request", "message": "Book already exist."}.to_json
