a2f0b2f0be634f49544ce681fe4cd481ee0a4f83
xHyroM <generalkubo@gmail.com>
2022-08-06 12:28:48 +0200

    feat(middleware/main): add header X-Hypilus-Language

--- hypilus-crystal/src/middleware/main.cr
+++ hypilus-crystal/src/middleware/main.cr
@@ -5,6 +5,7 @@ module MainMiddleware
         def call(env : HTTP::Server::Context)
             # Set my headers
             env.response.headers["X-Powered-By"] = "Kemal (Crystal)"
+            env.response.headers["X-Hypilus-Language"] = "Crystal (https://crystal-lang.org)"
     
             call_next env
         end
